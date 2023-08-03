84f83f8c1a8f538f0354873d29bd5211423fe1af
Andre Ferdinand <aferdina@mail.uni-mannheim.de>
2023-08-03 09:31:14 +0200

    clean up

--- multiarmedbandits/run_algorithm/train_multiarmed_bandits.py
+++ multiarmedbandits/run_algorithm/train_multiarmed_bandits.py
@@ -75,23 +75,20 @@ class RunMultiarmedBanditModel:
 
                 self.mab_algo.update(action, reward)
         # calculate needed metrics
-        mean_rewards_over_runs = np.mean(rewards, axis=0)
-        mean_cum_rewards_over_runs = np.cumsum(mean_rewards_over_runs)
+        mean_cum_rewards_over_runs = np.cumsum(np.mean(rewards, axis=0))
         mean_regret_over_runs = np.mean(regrets, axis=0)
         mean_optimalities_over_runs = np.mean(optimalities, axis=0)
         index_array = np.arange(len(mean_optimalities_over_runs)) + 1
-        optimalities_percentage_over_runs = mean_optimalities_over_runs / index_array
-        statistics = MABMetrics(
+        return MABMetrics(
             horizon=self.bandit_env.max_steps,
             no_runs=no_runs,
             regret=mean_regret_over_runs,
             optimalities=mean_optimalities_over_runs,
-            optim_percentage=optimalities_percentage_over_runs,
+            optim_percentage=mean_optimalities_over_runs / index_array,
             cum_reward=mean_cum_rewards_over_runs,
             average_reward=mean_cum_rewards_over_runs / index_array,
             regret_convergence=mean_regret_over_runs / index_array,
         )
-        return statistics
 
     def add_runs_to_metrics(self, metrics_to_add: MABMetrics) -> None:
         """add metric to the current metrics
@@ -100,51 +97,3 @@ class RunMultiarmedBanditModel:
             metrics_to_add (MABMetrics): metrics to add
         """
         self.metrics = self.metrics + metrics_to_add
-
-
-# def train_multiarmedrandom(agent, env, num_games, parameter, printed):
-#     """train/run multiarmed model on a game environment
-
-#     Args:
-#         agent (obj): agent, multiarmed model
-#         env (obj): game environment
-#         num_games (int): number of games to play
-#         parameter (str): string with name of releveant parameter
-#         printed (bool): bool if metrics should be printed
-
-#     Returns:
-#         list: list including all relevant metrics
-#     """
-
-#     chosen_arms = np.zeros(shape=(num_games, env.max_steps))
-#     rewards = np.zeros(shape=(num_games, env.max_steps))
-#     regrets = np.zeros(shape=(num_games, env.max_steps))
-#     optimalities = np.zeros(shape=(num_games, env.max_steps))
-
-#     for game in range(num_games):
-#         # playing the algo for `num_games` rounds
-#         agent.reset()
-#         env.reset()
-#         done = False
-#         while not done:
-#             # playing the game until it is done
-#             action = agent.select_arm(env.count)
-#             _new_state, reward, done, _info = env.step(action)
-#             rewards[game, (env.count - 1)] = reward
-#             chosen_arms[game, (env.count - 1)] = action
-#             regrets[game, (env.count - 1)] = env.regret
-#             optimalities[game, (env.count - 1)] = env.played_optimal
-
-#             agent.update(action, reward)
-
-#     if printed:
-#         plot_statistics(
-#             prin_rewards=rewards,
-#             prin_chosen_arms=chosen_arms,
-#             prin_regrets=regrets,
-#             prin_optimalities=optimalities,
-#             parameter=getattr(agent, parameter),
-#             name=parameter,
-#         )
-
-#     return rewards, chosen_arms, regrets, optimalities

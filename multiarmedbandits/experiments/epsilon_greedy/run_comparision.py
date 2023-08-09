346f1bad0cd4e7275d4e9b1182d8c41a5482fd60
Andre Ferdinand <aferdina@mail.uni-mannheim.de>
2023-08-09 17:31:02 +0200

    init comparision for epsilon greedy

---
+++ multiarmedbandits/experiments/epsilon_greedy/run_comparision.py
@@ -0,0 +1,55 @@
+from multiarmedbandits.run_algorithm.compare_models import (
+    CompareMultiArmedBandits,
+    MultiArmedBanditModel,
+    NamedMABMetrics,
+    Algorithms,
+)
+from multiarmedbandits.environments import (
+    BaseBanditEnv,
+    DistParameter,
+    ArmDistTypes,
+    INFODICT,
+)
+from multiarmedbandits.run_algorithm.utils import (
+    MetricNames,
+)
+
+# Define experiment configurations
+
+## env
+test_environment = BaseBanditEnv(
+    distr_params=DistParameter(
+        dist_type=ArmDistTypes.GAUSSIAN,
+        mean_parameter=[0.1, 0.7, 0.5],
+        scale_parameter=[1.0, 2.0, 3.0],
+    ),
+    max_steps=1000,
+)
+
+## Algorithms to compare
+algo_one = MultiArmedBanditModel(
+    dist_type=Algorithms.EPSILONGREEDY, dist_params={"epsilon": 0.1}
+)
+algo_two = MultiArmedBanditModel(
+    dist_type=Algorithms.EPSILONGREEDY, dist_params={"epsilon": 0.2}
+)
+algo_three = MultiArmedBanditModel(
+    dist_type=Algorithms.EPSILONGREEDY, dist_params={"epsilon": 0.3}
+)
+algo_four = MultiArmedBanditModel(
+    dist_type=Algorithms.EPSILONGREEDY, dist_params={"epsilon": 0.4}
+)
+explorethencommit_compare = CompareMultiArmedBandits(
+    test_env=test_environment,
+    mab_algorithms=[algo_one, algo_two, algo_three, algo_four],
+)
+metrics = explorethencommit_compare.train_all_models(no_of_runs=100)
+
+explorethencommit_compare.plot_multiple_mabs(
+    metrics_to_plot=[
+        MetricNames.AVERAGE_REWARD,
+        MetricNames.OPTIM_PERCENTAGE,
+        MetricNames.REGRETCONVERGENCE,
+    ],
+    named_metrics=metrics,
+)

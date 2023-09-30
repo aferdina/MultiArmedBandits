#32 fix beta-calculation for UCB

multiarmedbandits/algorithms/boltzmann.py
@@ -108,14 +108,14 @@ class BoltzmannSimple(BaseModel):
             case ExplorationType.UCB:
 
                 def _calc_betas(arm_attrib: ArmAttributes) -> np.ndarray:
-                    _square_counts = np.sqrt(self.counts)
+                    _sqrt_counts = np.sqrt(self.counts)
                     result = np.divide(
                         self.some_constant,
-                        _square_counts,
+                        _sqrt_counts,
                         out=np.zeros_like(self.some_constant),
-                        where=_square_counts != 0,
+                        where=_sqrt_counts != 0,
                     )
-                    result = result * np.log(1 + arm_attrib.step_in_game)
+                    result = result * np.sqrt(np.log(1 + arm_attrib.step_in_game))
                     result[result == 0.0] = np.inf
                     return result
 
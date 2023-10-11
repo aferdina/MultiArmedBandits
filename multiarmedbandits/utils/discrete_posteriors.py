2be802cbc8325c7fb37ee3901ea7420d6dd2a8d5
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-11 15:21:00 +0200

    Move posterior classes into discrete/contin file

---
+++ multiarmedbandits/utils/discrete_posteriors.py
@@ -0,0 +1,65 @@
+"""
+Collection of discrete posterior distributions.
+"""
+from typing import Any, Dict
+
+import numpy as np
+from scipy.stats import beta
+
+from multiarmedbandits.utils.abstract_posterior import AbstractPosterior
+
+
+def check_length_parameters(parameters, n_arms):
+    """
+    Check whether parameters have the same length as the arms of the multi-armed bandit.
+    """
+    assert len(parameters) == n_arms, f"There have to be {n_arms} initial parameter values."
+
+
+class BetaPosterior(AbstractPosterior):
+    """
+    Class for the posterior of a prior beta distribution (which is again beta distributed).
+
+    Per default we choose the Bayes prior Beta(1,1) for every arm.
+    It is mathematically equivalent to a uniform distribution.
+    """
+
+    def __init__(self, n_arms: int, config: Dict[str, Any]) -> None:
+        self.config = config
+        self.n_arms = n_arms
+        # Initialize the alpha parameter of the Beta distribution for each arm
+        if "alpha" in self.config:
+            check_length_parameters(self.config["alpha"], self.n_arms)
+            self.alpha = np.array(self.config["alpha"])
+        else:
+            self.alpha = np.ones(self.n_arms)
+        # Initialize the beta parameter of the Beta distribution for each arm
+        if "beta" in self.config:
+            check_length_parameters(self.config["beta"], self.n_arms)
+            self.beta = np.array(self.config["beta"])
+        else:
+            self.beta = np.ones(self.n_arms)
+
+    def sample(self) -> np.ndarray:
+        return beta.rvs(self.alpha, self.beta)  # Samples from the Beta distribution for each arm
+
+    def update(self, action: int, reward: int) -> None:
+        # Update the alpha or beta parameter for the selected arm, based on the received reward
+        if reward:
+            self.alpha[action] += 1
+        else:
+            self.beta[action] += 1
+
+    def reset(self) -> None:
+        # Reset the alpha parameter of the Beta distribution for each arm
+        if "alpha" in self.config:
+            check_length_parameters(self.config["alpha"], self.n_arms)
+            self.alpha = np.array(self.config["alpha"])
+        else:
+            self.alpha = np.ones(self.n_arms)
+        # Reset the beta parameter of the Beta distribution for each arm
+        if "beta" in self.config:
+            check_length_parameters(self.config["beta"], self.n_arms)
+            self.beta = np.array(self.config["beta"])
+        else:
+            self.beta = np.ones(self.n_arms)

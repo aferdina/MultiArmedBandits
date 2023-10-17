ebbe8f9394cdd2b11bb183ec2c1d98e9796e0990
Viktoria Hablas <viky@MacBook-Pro-von-Viktoria.local>
2023-10-17 17:41:08 +0200

    #41 Add Dash Application for configuring yaml for experiments

---
+++ multiarmedbandits/run_algorithm/trainings_configs/app.py
@@ -0,0 +1,40 @@
+from dash import Dash, dcc, html, Input, Output, callback
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H6("Change the value in the text box to see callbacks in action!"),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    
+    
+    html.Div(id='my-output'),
+    html.Br(),
+    html.Label('Select how what Algorithm you want to test:'),
+    dcc.Dropdown(['EpsilonGreedy', 'ExploreThenCommit', 'UCBAlpha', 'LectureUCB',
+                'BoltzmannSimple', 'BoltzmannGeneral', 'GradientBandit'], '1', id='demo-dropdown'),
+        html.Div(id='dd-output-container'),
+
+])
+
+
+@callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
+
+@callback(
+    Output('dd-output-container', 'children'),
+    Input('demo-dropdown', 'value')
+)
+def update_output_dropdown(value):
+    return f'You have selected {value}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ multiarmedbandits/run_algorithm/trainings_configs/test_VH.py
@@ -0,0 +1,93 @@
+import dash
+from dash import dash_table
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State, ALL
+import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
+
+
+app = dash.Dash(__name__, prevent_initial_callbacks=True, suppress_callback_exceptions=True)
+
+all_options = {
+    'EpsilonGreedy': ['epsilon', 'andererWert'],
+    'ExploreThenCommit': ['wert', 'wert', 'wert'],
+    'UCBAlpha': [],
+    'LectureUCB': [],
+    'BoltzmannSimple': [], 
+    'BoltzmannGeneral': [],
+    'GradientBandit': [],
+}
+
+
+def generate_inputs(config):
+    return html.Div([
+        str(config),
+        dcc.Input(id={
+                        'type': 'dynamic-textbox',
+                        'index': config
+                    }, placeholder=str(config), type='text')
+    ])
+
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='algo-dropdown',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='EpsilonGreedy'
+    ),
+    dbc.Button(id='btn-scrape',
+                           children=["Add to yaml  ", html.I(className="fa fa-download mr-1")],
+                           color="primary",
+                           className="mt-1"
+                           ),
+    html.Hr(),
+
+    html.Div(id='display-inputs', children=[generate_inputs(i) for i in all_options['EpsilonGreedy']]),
+
+    html.Hr(),
+    dbc.Col([
+        dcc.Loading(
+            id='loading-1',
+            type='circle',
+            children=[
+                dash_table.DataTable(
+                    id='datatable-paging',
+                    data=[{}],
+                    editable=True,
+                    style_cell={
+                        'whiteSpace': 'normal',
+                        'lineHeight': 'auto',
+                        'height': 'auto',
+                        'fontSize': 12},
+                    page_current=0,
+                    page_size=10,
+                    page_action='native'),
+                dcc.Download(id='download-table')
+            ]),
+    ])
+])
+
+@app.callback(
+    Output('display-inputs', 'children'),
+    Input('algo-dropdown', 'value'),
+)
+def set_configs_children(scraping_setting):
+    return dbc.Col(children=[generate_inputs(i) for i in all_options[scraping_setting]], className='placeholder')
+
+'''
+App callback: Scraping data from eurlex using the XmlScraper Class
+'''
+@app.callback(
+    Output('datatable-paging', 'data'),
+    [Input('btn-scrape', 'n_clicks')],
+    State({'type': 'dynamic-textbox', 'index': ALL}, 'value'),
+)
+def update_output(n_clicks, value):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        print(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
